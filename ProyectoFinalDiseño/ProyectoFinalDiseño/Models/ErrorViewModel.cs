ccdbefb0564b32536839f9951f40d8b2cead00d0
Daniel Sánchez <lightningsound@outlook.com>
2025-02-24 21:54:51 -0600

    Revert "Solucion en blanco"
    This reverts commit 8bce7b1e8200a753ab5411f247e5c9c99c62065a.

--- ProyectoFinalDiseño/ProyectoFinalDiseño/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProyectoFinalDiseño.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

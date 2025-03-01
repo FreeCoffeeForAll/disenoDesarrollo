4d9049cd751c9807ccfdf4f260fd681775e3961c
Daniel Sánchez <lightningsound@outlook.com>
2025-03-01 10:58:19 -0600

    Revert "Commit"
    This reverts commit 9001f34996302f4f21d801fa0818bdcfd0571b9e.

--- ToDo.Application/Contracts/Repositories/ITaskRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using ToDo.Domain.Entities;
-using Task = ToDo.Domain.Entities.Task;
-
-namespace ToDo.Application.Contracts.Repositories
-{
-    public interface ITaskRepository : IRepository<Task>
-    {
-    }
-}
--- ToDo.Infrastructure/Models/PocoModels/TaskPermission.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ToDo.Infrastructure.Models.PocoModels
-{
-    public class TaskPermission
-    {
-        public string Controller { get; set; }
-
-        public string Action { get; set; }
-    }
-}

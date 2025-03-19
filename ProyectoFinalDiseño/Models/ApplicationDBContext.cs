1866785a97af2a3b137693d830bb70a3533d637f
Daniel Sánchez <lightningsound@outlook.com>
2025-03-18 20:59:55 -0600

    Revert "db context"
    This reverts commit e2ee3457dd8552cafab88ad3c32afc88b0b8be2f.

--- ProyectoFinalDiseño/Models/ApplicationDBContext.cs
+++ ProyectoFinalDiseño/Models/ApplicationDBContext.cs
@@ -1,29 +1,51 @@
-﻿using System.Collections.Generic;
-using System.Data.Entity;
-using System.Reflection.Emit;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ProyectoFinalDiseño.Models
 {
-    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        // Add your application tables
+        public DbSet<Inventario> Inventario { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-        }
-
-        public Microsoft.EntityFrameworkCore.DbSet<Categoria> Categorias { get; set; }
-        public Microsoft.EntityFrameworkCore.DbSet<Inventario> Inventario { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Configuración de la relación entre Inventario y Categoría
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+            base.OnModelCreating(builder); // Ensure identity tables are created
+=======
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
             modelBuilder.Entity<Inventario>()
                 .HasOne(i => i.Categoria)
                 .WithMany()
-                .HasForeignKey(i => i.CategoriaID)
-                .OnDelete(DeleteBehavior.SetNull);
+                .HasForeignKey(i => i.CategoriaID);
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+=======
+
+            base.OnModelCreating(modelBuilder);
+        }
+>>>>>>> Stashed changes
+
+            base.OnModelCreating(modelBuilder);
+        }
+>>>>>>> Stashed changes
+
+            base.OnModelCreating(modelBuilder);
+        }
+>>>>>>> Stashed changes
+
+            // Define primary key for Categoria explicitly if necessary
+            builder.Entity<Categoria>().HasKey(c => c.CategoriaID);
         }
     }
 }

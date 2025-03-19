13ca53a03fd4ecc8b1ce07d77623c4f73d56fbaf
Daniel Sánchez <lightningsound@outlook.com>
2025-03-18 20:59:49 -0600

    Revert "Contenido v1"
    This reverts commit 5b39b7b53c43579a5c2442bfac8d367be6987a07.

--- ProyectoFinalDiseño/Models/ApplicationDBContext.cs
+++ ProyectoFinalDiseño/Models/ApplicationDBContext.cs
@@ -13,7 +13,6 @@ namespace ProyectoFinalDiseño.Models
 
         public Microsoft.EntityFrameworkCore.DbSet<Categoria> Categorias { get; set; }
         public Microsoft.EntityFrameworkCore.DbSet<Inventario> Inventario { get; set; }
-        public Microsoft.EntityFrameworkCore.DbSet<Contenido> Contenido { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
